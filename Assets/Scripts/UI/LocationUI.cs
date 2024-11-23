87f7b06b08cc99cb95d19e37da6d704ebfb02a87
Roman Shnaider <gsmartrsh@gmail.com>
2024-11-23 17:00:08 +0200

    Base for class LocationUI

--- Assets/Scripts/UI/LocationUI.cs
+++ Assets/Scripts/UI/LocationUI.cs
@@ -1,16 +1,38 @@
+using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
 
 public class LocationUI : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    [Header("Question")]
+    [SerializeField] private GameObject QuestionUI;
+    [SerializeField] private Button yesButtonQuestionUI;    
+    [SerializeField] private Button noButtonQuestionUI;    
+    [SerializeField] private TMP_Text messageTextQuestionUI;
+    private List<UnityEvent> yesEventsQuestionUI;           
+    private List<UnityEvent> noEventsQuestionUI;         
+    
+    [Header("TextResult")]
+    [SerializeField] private GameObject TextResultUI;
+    [SerializeField] private Button skipButtonTextResultUI;
+    [SerializeField] private TMP_Text messageTextResultUI;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    [Header("PathUI")]
+    [SerializeField] private GameObject PathUI;
+    [SerializeField] private ScrollController scrollControllerPathUI;    
+    [SerializeField] private GameObject tilePrefabPathUI;
+
+    [Header("ItemRewardUI")]
+    [SerializeField] private GameObject ItemRewardUI;
+    [SerializeField] private Transform placeForItemRewardUI;
+    [SerializeField] private  GameObject itemPrefabRewardUI;
+
+    [Header("ActionChoseUI")]
+    [SerializeField] private GameObject ActionChoseUI;
+    [SerializeField] private Button firstButtonQuestionUI;   
+    [SerializeField] private Button secondButtonQuestionUI;
+    private List<UnityEvent> firstEventsQuestionUI;           
+    private List<UnityEvent> secondEventsQuestionUI;          
 }
